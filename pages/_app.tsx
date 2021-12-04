3b593458d76c5ce3c555c04d7076173c13357f89
jakemeout <jacobosity@gmail.com>
2021-12-04 14:11:34 -0500

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../styles/ThemeProvider/ThemeProvider";
+import { useTheme } from "../styles/ThemeProvider/ThemeProvider";
+import styled from "styled-components";
+
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const { theme } = useTheme();
   return (
     <ThemeProvider>
       <Component {...pageProps} />
---
+++ pages/test.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+// import styled from "styled-components";
+
+const Test: NextPage = () => {
+
+
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+
+
+export default Test;
